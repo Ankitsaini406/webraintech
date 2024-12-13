9514966e41af8fddeaa1ec0186c6962816eec729
Ankit Saini <as.ankitsaini406@gmail.com>
2024-12-13 10:24:17 +0530

    "add images in site"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>We Brain Tech</h1>
+    <div className={styles.mainFlex}>
+      <main className={styles.imageBox}>
+        <Image 
+        className={styles.image}
+        src='/images/front_image.jpg'
+        alt="Front Image"
+        width={600}
+        height={800}
+        >
+        </Image>
+
+        <Image
+        className={styles.image}
+        src='/images/back_image.jpg'
+        alt="Back Image"
+        width={600}
+        height={800}
+        >
+        </Image>
       </main>
     </div>
   );
