6eaa1c7d813ed887433b754f81b2ab5d0ad15109
Ankit Saini <as.ankitsaini406@gmail.com>
2024-12-28 21:40:47 +0530

    "footer is created and hover is done"

--- src/components/featureSection.tsx
+++ src/components/featureSection.tsx
@@ -4,7 +4,7 @@ function FeatureSection() {
 
     return (
         <div className="container my-7 p-4 sm:p-8 flex flex-col-reverse sm:flex-row gap-4">
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4 w-full">
                 <p className="text-red-500">A Vision for Next-Gen</p>
                 <h2 className="text-4xl sm:text-5xl font-bold mb-6">Medium length section heading goes here</h2>
                 <p className="pb-0 sm:pb-4">Our courses are designed to equip you with practical skills and knowledge. Whether you are a beginner or looking to advance, we have something for everyone.</p>
@@ -14,7 +14,7 @@ function FeatureSection() {
                     <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
                 </ul>
             </div>
-            <div className="border bg-gradient-to-b from-transparent to-black/30 aspect-square w-full">
+            <div className="border bg-gradient-to-b from-transparent to-black/30 aspect-video w-full">
             </div>
         </div>
     )
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,50 @@
+import { Facebook, Instagram, Linkedin, X, Youtube } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 
 function Footer() {
     return (
-        <footer className="w-full bg-black text-white p-4">
-            <p>Footer</p>
+        <footer className="w-full bg-background dark:bg-black bg-neutral-200 p-4 sm:p-8">
+            <div className="flex flex-col gap-8 sm:flex-row justify-around items-center sm:items-start">
+                <div className="flex flex-col gap-2 sm:gap-4 items-center sm:items-start">
+                <Image className="h-full" src='/favicon.ico' alt="WeBrainTech" width={100} height={100} />
+                <div className="flex flex-col gap-2 sm:gap-4 items-center sm:items-start">
+                    <h5 className="font-bold text-xl sm:text-2xl">Follow Us</h5>
+                    <div className="flex flex-row gap-4 sm:gap-8">
+                    <Facebook className="cursor-pointer hover:text-red-500 transition-all duration-300"/>
+                    <Instagram className="cursor-pointer hover:text-red-500 transition-all duration-300"/>
+                    <X className="cursor-pointer hover:text-red-500 transition-all duration-300"/>
+                    <Linkedin className="cursor-pointer hover:text-red-500 transition-all duration-300"/>
+                    <Youtube className="cursor-pointer hover:text-red-500 transition-all duration-300"/>
+                    </div>
+                </div>
+                </div>
+                <div className="flex flex-col gap-2 sm:gap-4 items-center sm:items-start">
+                    <h5 className="font-bold text-xl sm:text-2xl">Ouick Links</h5>
+                    <Link className="hover:text-red-500 transition-all duration-300" href='/' >Abouts Us</Link>
+                    <Link className="hover:text-red-500 transition-all duration-300" href='/' >Contact Us</Link>
+                    <Link className="hover:text-red-500 transition-all duration-300" href='/' >Support Center</Link>
+                    <Link className="hover:text-red-500 transition-all duration-300" href='/' >Blog Post</Link>
+                    <Link className="hover:text-red-500 transition-all duration-300" href='/' >Careers</Link>
+                </div>
+                <div className="flex flex-col gap-2 sm:gap-4 items-center sm:items-start">
+                    <h5 className="font-bold text-xl sm:text-2xl">Connect With Us</h5>
+                    <Link className="hover:text-red-500 transition-all duration-300" href='/' >Events</Link>
+                    <Link className="hover:text-red-500 transition-all duration-300" href='/' >Testimonials</Link>
+                    <Link className="hover:text-red-500 transition-all duration-300" href='/' >Resources</Link>
+                    <Link className="hover:text-red-500 transition-all duration-300" href='/' >FAQs</Link>
+                </div>
+            </div>
+            <hr className="my-4 sm:my-6 dark:border-white border-black"></hr>
+            <div className="flex justify-between flex-col-reverse items-center gap-4 sm:gap-0 sm:flex-row">
+                <p className="sm:text-sm text-center text-xs">© 2024 WeBrainTech. All rights reserved.</p>
+                <div className="flex flex-col sm:flex-row gap-2 sm:gap-6">
+                    <Link className="underline underline-offset-2 hover:text-red-500 hover:underline hover:underline-offset-4 transition-all duration-300" href='/' >Privacy Policy</Link>
+                    <Link className="underline underline-offset-2 hover:text-red-500 hover:underline hover:underline-offset-4 transition-all duration-300" href='/' >Terms of Service</Link>
+                    <Link className="underline underline-offset-2 hover:text-red-500 hover:underline hover:underline-offset-4 transition-all duration-300" href='/'>Cookies Settings</Link>
+                </div>
+            </div>
         </footer>
     );
 }
