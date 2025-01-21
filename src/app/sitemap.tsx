a3f5b66d6abd84e400e59c3671f5cd4aa548a05e
Ankit Saini <as.ankitsaini406@gmail.com>
2025-01-21 11:12:11 +0530

    "changes in sitemap file"

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,56 +1,52 @@
 
-// export default async function sitemap() {
+export default async function sitemap() {
 
-//     const apiPoint = process.env.NODE_ENV === "development" ? process.env.API_URL : process.env.HOST_URL;
+    const apiPoint = process.env.NODE_ENV === "development" ? process.env.API_URL : process.env.HOST_URL;
 
-//     const tourResponse = await fetch(`${apiPoint}api/group-tours`);
-//     if (!tourResponse.ok) {
-//         throw new Error("Failed to fetch tour data");
-//     }
-//     const tours = await tourResponse.json();
+    // const tourResponse = await fetch(`${apiPoint}api/group-tours`);
+    // if (!tourResponse.ok) {
+    //     throw new Error("Failed to fetch tour data");
+    // }
+    // const tours = await tourResponse.json();
 
-//     const blogResponse = await fetch(`${apiPoint}api/blog`);
-//     if (!blogResponse.ok) {
-//         throw new Error("Failed to fetch blog data");
-//     }
-//     const blog = await blogResponse.json();
+    // const blogResponse = await fetch(`${apiPoint}api/blog`);
+    // if (!blogResponse.ok) {
+    //     throw new Error("Failed to fetch blog data");
+    // }
+    // const blog = await blogResponse.json();
 
-//     const tourDetails = tours?.map((tour) => {
-//         return {
-//             url: `${apiPoint}group-tours/${tour?.slug}`,
-//             lastModified: tour?.createdAt,
-//         };
-//     });
+    // const tourDetails = tours?.map((tour) => {
+    //     return {
+    //         url: `${apiPoint}group-tours/${tour?.slug}`,
+    //         lastModified: tour?.createdAt,
+    //     };
+    // });
 
-//     const blogDetails = blog?.map((blog) => {
-//         return {
-//             url: `${apiPoint}blog/${blog?.slug}`,
-//             lastModified: blog?.createdAt,
-//         }
-//     })
+    // const blogDetails = blog?.map((blog) => {
+    //     return {
+    //         url: `${apiPoint}blog/${blog?.slug}`,
+    //         lastModified: blog?.createdAt,
+    //     }
+    // })
 
-//     const staticPages = [
-//         {
-//             url: `${apiPoint}group-tour`,
-//             lastModified: new Date().toISOString(),
-//         },
-//         {
-//             url: `${apiPoint}about`,
-//             lastModified: new Date().toISOString(),
-//         },
-//         {
-//             url: `${apiPoint}contact-us`,
-//             lastModified: new Date().toISOString(),
-//         },
-//     ];
+    const staticPages = [
+        {
+            url: `${apiPoint}about-us`,
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: `${apiPoint}contact-us`,
+            lastModified: new Date().toISOString(),
+        },
+    ];
 
-//     return [
-//         {
-//             url: `${apiPoint}`,
-//             lastModified: new Date(),
-//         },
-//         ...tourDetails,
-//         ...blogDetails,
-//         ...staticPages,
-//     ]
-// }
+    return [
+        {
+            url: `${apiPoint}`,
+            lastModified: new Date(),
+        },
+        // ...tourDetails,
+        // ...blogDetails,
+        ...staticPages,
+    ]
+}
