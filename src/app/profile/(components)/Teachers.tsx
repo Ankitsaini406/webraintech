efe0292a1cb8492b50766822f4f54641285befbf
Ankit Saini <as.ankitsaini406@gmail.com>
2025-01-28 15:42:12 +0530

    "changes in profile data"

---
+++ src/app/profile/(components)/Teachers.tsx
@@ -0,0 +1,8 @@
+
+export default function Teachers() {
+    return (
+        <div className="felx flex-col">
+            
+        </div>
+    );
+}
--- src/app/profile/ProfileComponent.tsx
+++ src/app/profile/ProfileComponent.tsx
@@ -8,12 +8,14 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { logoutStudent } from "@/store/slices/userSlice";
 import { useRouter } from "next/navigation";
 import { Users } from "@/utils/InitialState";
+import Dashboard from "./(components)/Dashboard";
+import Teachers from "./(components)/Teachers";
 
 interface ProfileComponentProps {
     user: Users | null;
 }
 
-export function ProfileComponent({user} : ProfileComponentProps) {
+export function ProfileComponent({ user }: ProfileComponentProps) {
     const [activeTab, setActiveTab] = useState("dashboard");
     const dispatch = useDispatch<AppDispatch>();
     const router = useRouter();
@@ -31,13 +33,13 @@ export function ProfileComponent({user} : ProfileComponentProps) {
 
         switch (activeTab) {
             case "dashboard":
-                return <div>Welcome to your dashboard! Here you can manage your activities.</div>;
-            case "teachers":
-                if (userRole === "students") {
-                    return <div>Here is the list of teachers.</div>;
+                return <Dashboard />;
+            case "teacher":
+                if (userRole === "student") {
+                    return <Teachers />;
                 }
                 return <div>Unauthorized to view this content.</div>;
-            case "students":
+            case "student":
                 if (userRole === "teacher") {
                     return <div>Here is the list of students.</div>;
                 }
@@ -45,12 +47,14 @@ export function ProfileComponent({user} : ProfileComponentProps) {
             case "account":
                 return <div>Update your account information here.</div>;
             case "signout":
-                return <button
-                    onClick={handleSignout}
-                    className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-                >
-                    Sign Out
-                </button>;
+                return (
+                    <button
+                        onClick={handleSignout}
+                        className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                    >
+                        Sign Out
+                    </button>
+                );
             default:
                 return <div>Select a tab to view content.</div>;
         }
@@ -61,14 +65,24 @@ export function ProfileComponent({user} : ProfileComponentProps) {
             {/* Profile Tabs */}
             <div className="border rounded p-4 h-full">
                 <div className="flex gap-4 flex-col items-center">
-                    {
-                        user === null ? <Skeleton className="w-[200px] aspect-square rounded-full" /> : <div className="flex items-center justify-center w-[200px] aspect-square rounded-full bg-gray-400 shadow-md">
+                    {user === null ? (
+                        <Skeleton className="w-[200px] aspect-square rounded-full" />
+                    ) : (
+                        <div className="flex items-center justify-center w-[200px] aspect-square rounded-full bg-gray-400 shadow-md">
                             <h1 className="text-white text-9xl">{firstLetter}</h1>
                         </div>
-                    }
+                    )}
                     <div className="flex flex-col gap-1 items-center">
-                        {user === null ? <Skeleton className="h-8 w-[250px]" /> : <h2 className="text-2xl font-bold">Howdy! {user.name}</h2>}
-                        {user === null ? <Skeleton className="h-6 w-[300px]" /> : <h2 className="text-slate-400">{user.email}</h2>}
+                        {user === null ? (
+                            <Skeleton className="h-8 w-[250px]" />
+                        ) : (
+                            <h2 className="text-2xl font-bold">Howdy! {user.name}</h2>
+                        )}
+                        {user === null ? (
+                            <Skeleton className="h-6 w-[300px]" />
+                        ) : (
+                            <h2 className="text-slate-400">{user.email}</h2>
+                        )}
                     </div>
 
                     <div className="flex flex-col gap-4 w-2/3">
@@ -83,14 +97,20 @@ export function ProfileComponent({user} : ProfileComponentProps) {
                             Dashboard
                         </button>
                         <button
-                            onClick={() => setActiveTab("teachers")}
-                            className={`flex gap-2 font-bold w-full py-2 px-4 rounded duration-300 ${activeTab === "teachers"
+                            onClick={() => {
+                                if (user?.role === "student") {
+                                    setActiveTab("teacher");
+                                } else {
+                                    setActiveTab("student");
+                                }
+                            }}
+                            className={`flex gap-2 font-bold w-full py-2 px-4 rounded duration-300 ${activeTab === "teacher" || activeTab === "student"
                                 ? "bg-red-500 text-white shadow-md"
                                 : "hover:bg-red-100 hover:shadow-md dark:hover:text-black"
                                 }`}
                         >
                             <User />
-                            Teachers
+                            {user?.role === "student" ? "Teachers" : "Students"}
                         </button>
                         <button
                             onClick={() => setActiveTab("account")}
