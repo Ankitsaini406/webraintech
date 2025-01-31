9a2e823fdf184400f8f46d9fa0c80111e46d25ee
Ankit Saini <as.ankitsaini406@gmail.com>
2025-01-31 15:16:33 +0530

    "changes in LoginComponent"

--- src/app/auth/(components)/LoginComponent.tsx
+++ src/app/auth/(components)/LoginComponent.tsx
@@ -41,29 +41,29 @@ const LoginComponent = () => {
                         <Link href='/'>
                             <Image alt="WeBrainTech" src='/favicon-192.png' fill />
                         </Link>
-                </div>
-                <div className="bg-background dark:bg-black p-8 w-full h-max">
-                    <h1 className="text-4xl font-semibold text-center mb-6 dark:text-white">
-                        Welcome Back
-                    </h1>
-                    <form className="w-full" onSubmit={handleSubmit}>
-                        <Input
-                            title="Email"
-                            name="email"
-                            type="email"
-                            value={formData.email}
-                            onChange={handleInputChange}
-                            className="rounded-none" />
-                        <Password
-                            title="Password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleInputChange}
-                            className="rounded-none" />
-                        {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
-                        <FormButton title={loading ? "Please Wait" : "Sign In"} disabled={loading} className="rounded-none !bg-black dark:!bg-neutral-800 dark:hover:!shadow-slate-500 hover:!shadow-lg mt-6 !text-white !w-full" />
-                    </form>
-                </div>
+                    </div>
+                    <div className="bg-background dark:bg-black p-8 w-full h-max">
+                        <h1 className="text-4xl font-semibold text-center mb-6 dark:text-white">
+                            Welcome Back
+                        </h1>
+                        <form className="w-full" onSubmit={handleSubmit}>
+                            <Input
+                                title="Email"
+                                name="email"
+                                type="email"
+                                value={formData.email}
+                                onChange={handleInputChange}
+                                className="rounded-none" />
+                            <Password
+                                title="Password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleInputChange}
+                                className="rounded-none" />
+                            {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
+                            <FormButton title={loading ? "Please Wait" : "Sign In"} disabled={loading} className="rounded-none !bg-black dark:!bg-neutral-800 dark:hover:!shadow-slate-500 hover:!shadow-lg mt-6 !text-white !w-full" />
+                        </form>
+                    </div>
                 </div>
                 <div className="hidden sm:block bg-slate-950 p-8 w-full h-full">
                 </div>
