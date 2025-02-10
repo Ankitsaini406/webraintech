9b64f009f7da72234b1d469ac49582c6834e2ff6
Ankit Saini <as.ankitsaini406@gmail.com>
2025-02-10 14:52:56 +0530

    "changes in privacy policy"

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -36,7 +36,7 @@ export default function PrivacyPolicy() {
             <h4 className="font-bold">Last Upddate: 10-Feb-2025</h4>
             <p className="py-4 ">Welcome to Webraintech Academy. Your privacy is critically important to us. This Privacy Policy explains how we collect, use, and protect your personal information when you visit our website, use our services, or interact with us.</p>
 
-            <h2 className="font-bold text-xl sm:text-3xl">1. Information We Collect</h2>
+            <h2 className="text-2xl font-semibold">1. Information We Collect</h2>
             <p className="py-4">We collect different types of information in various ways to provide and improve our services:</p>
 
             <h3 className="pl-4 text-xl">A. Personal Information</h3>
@@ -63,7 +63,7 @@ export default function PrivacyPolicy() {
             <h3 className="pl-4 text-xl">C. Cookies and Tracking Technologies</h3>
             <p className="py-4 pl-6">We use cookies and similar tracking technologies to enhance user experience and analyze website traffic.</p>
 
-            <h2 className="font-bold text-xl sm:text-3xl">2. How We Use Your Information</h2>
+            <h2 className="text-2xl font-semibold">2. How We Use Your Information</h2>
             <p className="py-4 pl-6">We use the collected information for the following purposes:</p>
             <ul className="pb-4 pl-6">
                 <li className="text-sm">- To provide, maintain, and improve our services</li>
@@ -74,16 +74,16 @@ export default function PrivacyPolicy() {
                 <li className="text-sm">- To enhance security and prevent fraud</li>
             </ul>
 
-            <h2 className="font-bold text-xl sm:text-3xl">3. How We Share Your Information</h2>
+            <h2 className="text-2xl font-semibold">3. How We Share Your Information</h2>
             <p className="py-4 pl-6">We do not sell, trade, or rent personal information. However, we may share your data with:</p>
             <p className="pl-6"><span className="font-bold">Service Providers:</span>Third-party vendors assisting in operations such as payment processing, hosting, and analytics</p>
             <p className="pl-6"><span className="font-bold">Legal Compliance:</span>Authorities when required by law or to protect our legal rights</p>
             <p className="pb-4 pl-6"><span className="font-bold">Business Transfers:</span>In case of a merger, acquisition, or asset sale</p>
 
-            <h2 className="font-bold text-xl sm:text-3xl">4. Data Security</h2>
+            <h2 className="text-2xl font-semibold">4. Data Security</h2>
             <p className="py-4 pl-6">We implement robust security measures to protect your personal data from unauthorized access, alteration, or disclosure. However, no method of data transmission over the internet is 100% secure.</p>
 
-            <h2 className="font-bold text-xl sm:text-3xl">5. Your Rights and Choices</h2>
+            <h2 className="text-2xl font-semibold">5. Your Rights and Choices</h2>
             <p className="py-4 pl-6">Depending on your location, you may have the right to:</p>
             <ul className="pb-4 pl-6">
                 <li className="text-sm">- Access, update, or delete your personal data</li>
@@ -94,16 +94,16 @@ export default function PrivacyPolicy() {
             </ul>
             <p className="py-4 pl-6">For any such requests, please contact us at *[Insert Contact Email]</p>
 
-            <h2 className="font-bold text-xl sm:text-3xl">6. Third-Party Links</h2>
+            <h2 className="text-2xl font-semibold">6. Third-Party Links</h2>
             <p className="py-4 pl-6">Our website may contain links to third-party websites. We are not responsible for their privacy policies or practices.</p>
 
-            <h2 className="font-bold text-xl sm:text-3xl">7. Children&apos;s Privacy</h2>
+            <h2 className="text-2xl font-semibold">7. Children&apos;s Privacy</h2>
             <p className="py-4 pl-6">Our services are not intended for children under 13 years of age. We do not knowingly collect personal data from minors.</p>
 
-            <h2 className="font-bold text-xl sm:text-3xl">8. Changes to This Privacy Policy</h2>
+            <h2 className="text-2xl font-semibold">8. Changes to This Privacy Policy</h2>
             <p className="py-4 pl-6">We reserve the right to modify this Privacy Policy at any time. Changes will be posted on this page with an updated effective date.</p>
 
-            <h2 className="font-bold text-xl sm:text-3xl">9. Contact Us</h2>
+            <h2 className="text-2xl font-semibold">9. Contact Us</h2>
             <p className="py-4 pl-6">For any questions regarding this Privacy Policy, please contact us at:</p>
 
             <h4 className="font-bold pb-4">Webraintech Academy</h4>
