334af3546ffa2d35797f2a21b3453d74c2938b32
Ankit Saini <as.ankitsaini406@gmail.com>
2025-02-15 15:27:33 +0530

    "change slug"

--- src/app/api/courses/[slug]/route.tsx
+++ src/app/api/courses/[slug]/route.tsx
@@ -1,13 +1,9 @@
 import prisma from "@/lib/db";
-import { NextResponse } from "next/server";
-import { Prisma } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
-type Params = { params: { slug: string } };
-
-export async function GET(req: Request, { params }: Params) {
+export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
     try {
         const { slug } = await params;
-
         const course = await prisma.course.findUnique({
             where: { slug },
             include: {
@@ -18,31 +14,12 @@ export async function GET(req: Request, { params }: Params) {
         });
 
         if (!course) {
-            return NextResponse.json(
-                { success: false, message: "Course not found" },
-                { status: 404 }
-            );
+            return NextResponse.json({ success: false, message: "Course not found"}, { status: 404 });
         }
 
-        return NextResponse.json(
-            { success: true, data: course },
-            { status: 200 }
-        );
-
+        return NextResponse.json({ success: true, data: course }, { status: 200 });
     } catch (error) {
-        console.error("❌ Error fetching course:", error);
-
-        // Handle Prisma-specific errors
-        if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            return NextResponse.json(
-                { success: false, message: `Database Error: ${error.message}` },
-                { status: 500 }
-            );
-        }
-
-        return NextResponse.json(
-            { success: false, message: "Internal Server Error" },
-            { status: 500 }
-        );
+        console.log(`Failed to fetch course : `,  error);
+        return NextResponse.json({ success: false, message: "Failed to fetch course"}, { status: 500 });
     }
 }
