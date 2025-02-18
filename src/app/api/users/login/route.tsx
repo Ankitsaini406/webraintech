c26832a1ea6b29624c822b0ac3562c058516ace8
Ankit Saini <as.ankitsaini406@gmail.com>
2025-02-18 11:01:10 +0530

    "changes in api"

--- src/app/api/users/login/route.tsx
+++ src/app/api/users/login/route.tsx
@@ -26,7 +26,6 @@ export async function POST(req: Request) {
             name: user.name,
             email: user.email,
             role: user.role,
-            course: user.course,
         });
 
         setCookie("authToken", token, { maxAge: 60 * 60 * 24 * 7 });
