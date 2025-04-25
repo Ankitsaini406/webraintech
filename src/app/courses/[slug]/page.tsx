6f11b6710ab191bfeca6b1231ef347cfdf2df275
Ankit Saini <as.ankitsaini406@gmail.com>
2025-04-25 20:04:38 +0530

    slug error solved

--- src/app/courses/[slug]/page.tsx
+++ src/app/courses/[slug]/page.tsx
@@ -1,12 +1,11 @@
 import { getCourse } from "@/actions/GetCourse";
 import CoursePage from "./Course";
 
-interface Props {
+interface PageProps {
     params: { slug: string };
 }
 
-export default async function Page({ params }: Props) {
+export default async function Page({ params }: PageProps) {
     const course = await getCourse(params.slug);
-
     return <CoursePage course={course} />;
 }
