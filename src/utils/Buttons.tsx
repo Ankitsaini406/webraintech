26f417ba57f925679832f26ce2ee90fdb1d9795f
Ankit Saini <as.ankitsaini406@gmail.com>
2025-04-26 12:26:14 +0530

    changes in colors and data

--- src/utils/Buttons.tsx
+++ src/utils/Buttons.tsx
@@ -15,9 +15,10 @@ interface ButtonBlackProps {
 export function ButtonWhite({ title, className, isLoading = false }: ButtonBlackProps) {
     return (
         <Button
+            variant='outline'
             disabled={isLoading}
             className={cn(
-                "p-2 px-8 border border-black dark:border-white bg-white dark:bg-black dark:text-white dark:hover:bg-white dark:hover:border-black dark:hover:text-black hover:bg-black hover:text-white text-black transition-all duration-300 transform hover:scale-105",
+                "p-2 px-8 border transition-all duration-300 transform hover:scale-105",
                 className)}
         >
             {isLoading && <svg className="mr-3 size-5 animate-spin ..." viewBox="0 0 24 24"></svg>}
@@ -29,9 +30,10 @@ export function ButtonWhite({ title, className, isLoading = false }: ButtonBlack
 export function ButtonBlack({ title, className, isLoading = false }: ButtonBlackProps) {
     return (
         <Button
+            variant='default'
             disabled={isLoading}
             className={cn(
-                "p-2 px-8 border border-transparent bg-black dark:bg-[ghostwhite] dark:text-black dark:hover:bg-black dark:hover:border-white dark:hover:text-white hover:bg-[ghostwhite] hover:text-black hover:border-black text-white transition-all duration-300 transform hover:scale-105",
+                "p-2 px-8 border border-transparent transition-all duration-300 transform hover:scale-105",
                 className
             )}
         >
