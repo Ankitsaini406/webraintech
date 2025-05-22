4d65d76153087d762f0932c0b45f6d9bb5acef78
Ankit Saini <as.ankitsaini406@gmail.com>
2025-05-22 13:57:07 +0530

    update placement images to webp format

--- src/app/placement/Placement.tsx
+++ src/app/placement/Placement.tsx
@@ -8,42 +8,42 @@ import { useState } from "react";
 
 const levrageData = [
     {
-        image: '',
+        image: '/placement/expertise.webp',
         title: 'Industry-Ready Talent',
         descriprtion: 'Our students undergo comprehensive training and projects to ensure they are prepared for real-world challenges.',
     },
     {
-        image: '',
+        image: '/placement/personalized.webp',
         title: 'Personalized Recruitment ',
         descriprtion: 'We assist recruiters throughout the hiring process, from pre-screening to scheduling interviews, making recruitment hassle-free.',
     },
     {
-        image: '',
+        image: '/placement/talent-management.webp',
         title: 'Diverse Talent Pool',
         descriprtion: 'We offer candidates from multipledisciplines including IT,management, commerce, and more,catering to varied hiring needs.',
     },
     {
-        image: '',
+        image: '/placement/foundation.webp',
         title: 'Strong Industry Connections',
         descriprtion: 'Our placement cell maintains longterm relationships with top companies, ensuring quality placement opportunities.',
     },
     {
-        image: '/placement/provision.png',
+        image: '/placement/provision.webp',
         title: 'Flexible Recruitment Options',
         descriprtion: 'Whether virtual or on-campus drives, we adapt to your preferred hiring method for maximum convenience.',
     },
     {
-        image: '/placement/dedication.png',
+        image: '/placement/dedication.webp',
         title: 'Dedicated Placement Team',
         descriprtion: 'Our experienced placement officers are committed to providing timely assistance and ensuring smooth coordination.',
     },
     {
-        image: '/placement/shopping.png',
+        image: '/placement/shopping.webp',
         title: 'Proven Placement Record',
         descriprtion: 'We have a strong track record of successful placements across various industries, reflecting the quality and readiness.',
     },
     {
-        image: '/placement/hiring.png',
+        image: '/placement/hiring.webp',
         title: 'Zero Placement Fees',
         descriprtion: 'Our services are completely free for recruiters, fostering a win-win partnership without any financial burden.',
     },
@@ -88,8 +88,8 @@ export default function Placement() {
                     <div className="relative h-[200px] sm:h-[400px] aspect-square">
                         <Image src='/placement/placement.webp' alt="Placement Image" fill />
                     </div>
-                    <h1 className="font-bold text-2xl md:text-4xl lg:text-5xl pb-4 uppercase text-center text-white max-w-[900px]">Welcome to the placement cell bridging talent and industry.</h1>
-                    <p className="pt-4 pb-15 leading-5 lg:leading-8 font-light md:text-lg lg:text-3xl text-white text-center max-w-[1000px]">We build strong industry-academia relations to ensure successful
+                    <h1 className="font-bold text-2xl md:text-4xl lg:text-5xl pb-4 uppercase text-center max-w-[900px]">Welcome to the placement cell bridging talent and industry.</h1>
+                    <p className="pt-4 pb-15 leading-5 lg:leading-8 font-light md:text-lg lg:text-3xl text-center max-w-[1000px]">We build strong industry-academia relations to ensure successful
                         placements for our students and valuable hires for your organization.
                         Let&apos;s work together to create career opportunities.</p>
                 </div>
@@ -101,8 +101,8 @@ export default function Placement() {
             {/* HR Section */}
             <div className="lg:container mx-auto p-4 flex flex-col md:flex-row gap-12 md:gap-0 justify-around items-center ">
                 <div className="relative h-[300px] lg:h-[330px] xl:h-[400px] w-1/2 sm:w-1/3 lg:w-1/5">
-                    <Image src='/placement/hr-background.png' alt='Hr Image' fill className="z-10 !left-5" />
-                    <Image src='/placement/hr-image.png' alt='Hr Image' fill className="z-10 !w-[200px] lg:!w-[250px]" />
+                    <Image src='/placement/hr-background.webp' alt='Hr Image' fill className="z-10 !left-5" />
+                    <Image src='/placement/hr-image.webp' alt='Hr Image' fill className="z-10 !w-[200px] lg:!w-[250px]" />
                 </div>
 
                 <div className="flex flex-col gap-8">
@@ -120,10 +120,10 @@ export default function Placement() {
             <h4 className="text-center text-xl pb-5">we are committed to building strong industry relationships and providing companies <br />
                 with access to highly skilled, motivated, and job-ready talent</h4>
 
-            <div className="lg:container mx-auto p-4 my-10 grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div className="lg:container mx-auto p-4 md:my-10 grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 {
                     levrageData.map((item) => (
-                        <div key={item.title} className="p-5 rounded-lg bg-accent-foreground flex flex-col gap-5">
+                        <div key={item.title} className="p-5 rounded-lg dark:bg-gray-300 flex flex-col gap-5 border">
                             <div className="relative w-12 aspect-square">
                                 <Image src={item.image} alt={item.title} fill className="shadow p-1 rounded-lg" />
                             </div>
